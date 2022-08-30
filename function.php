936340bca72f185515770fd15f6d25148cfa3d21
divyanshagarwall <divyanshagrwl.2@gmail.com>
2022-08-30 08:54:56 +0530

    main

--- function.php
+++ function.php
@@ -1,7 +1,33 @@
 <?php
 
+// asci valuec learn 
+
 // echo addcslashes("hello class","hsc");
 // echo addslashes("hel'lo");
 // echo bin2hex("h");
 // echo chunk_split("hello section k",1,".");
+// echo chop("hello php","p");
+// echo rtrim();
+// echo trim("hello","h");
+// echo chr(67);
+// echo ord("C");
+
+//function
+// echo convert_cyr_string("hello","h","o"); only till php 7
+
+// $res = convert_uuencode("akash");
+// echo convert_uuencode($res);
+
+// echo print printf sprintf 4 function already read
+// print r does not show datatype and length of array while var dump show
+
+// $arr=["23","4","5","88"];
+// var_dump($arr);
+// echo
+// print_r($arr);
+
+// count_chars have 3 modes 1 2 3 by default it uses mod 2
+
+// print_r(count_chars("AbcdAAB",1));
+// print_r(count_chars("AbcdAAB",3));
 
