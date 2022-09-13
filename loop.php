7073783145de929ed920101d0b5cdb367f1686ff
divyanshagarwall <divyanshagrwl.2@gmail.com>
2022-09-13 09:02:33 +0530

    main

---
+++ loop.php
@@ -0,0 +1,26 @@
+<?php
+
+// $count = 1; //loop ounter initialization
+
+// while($count <= 10) //test condition
+// {
+//     echo"$count = divyansh"."<br>";
+//     $count++; //counter increament&decreament
+// }
+
+// do
+// {
+//     echo"$count = divyansh"."<br>";
+//     $count++; //counter increament&decreament
+// }
+// while($count <= 100); //test condition
+
+
+
+
+// $count = 1;
+// for(; $count <= 100; $count++)
+// {
+//     echo $count."<br>";
+// }
+
