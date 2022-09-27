8119b6e285b4c0c81da42652b48ff151e9e4e150
divyanshagarwall <divyanshagrwl.2@gmail.com>
2022-09-27 11:06:18 +0530

    main

--- file.php
+++ file.php
@@ -1,4 +1,5 @@
 <?php
-$val_one=$_POST["num_one"]
-$val_two=$_POST["num_two"]
-echo $val_one+$val_two
+$val_one = $_POST['num_one'];
+$val_two = $_POST['num_two'];
+$result = $val_one + $val_two;
+echo "The sum of two number $val_one and $val_two is :<input type=text value=$result readonly>";
