ec3aefcf187d0c48d8512d4fecb9835cd563293c
divyanshagarwall <divyanshagrwl.2@gmail.com>
2022-11-14 14:48:35 +0530

    14/11/2022

---
+++ section_k.php
@@ -0,0 +1,23 @@
+<?php
+$handle = fopen("sectionk.txt","w");
+fwrite($handle,"Welcome \n Class");
+fclose($handle);
+$handle = fopen("sectionk.txt","r");
+//$data = fread($handle,filesize("sectionk,txt"));
+// echo str_replace("\n","<br>",$data);
+// echo fgets($handle);
+while(!feof($handle))
+{
+    $data = fgetc($handle);
+    if($data == "\n")
+    {
+        $data = "<br>";
+    }
+    echo $data;
+}
+copy ("sectionk.txt","sectionk2.txt");
+if(file_exists("sectionk2.txt") or die("file not exists"))
+{
+    echo "YES";
+}
+// unlink("sectionk2.txt");
